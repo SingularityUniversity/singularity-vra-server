6dc56d100fd981bc96b6940c00e6dd43e4695c1d
Gabe Wachob <gabe@tapcentive.com>
2016-04-11 14:47:26 -0700

    Added rss and url ingestion as tasks

--- core/tasks.py
+++ core/tasks.py
@@ -1,5 +1,17 @@
 from celery import shared_task
+from ingestion.rss import ingest_rss
+from ingestion.content import ingest
 
 @shared_task
 def test(param):
     return 'The test task executed with argument "{}" '.format(param)
+
+
+@shared_task
+def add_rss(rss_url):
+    return ingest_rss(rss_url)
+
+
+@shared_task
+def add_content(url):
+    return ingest(url)

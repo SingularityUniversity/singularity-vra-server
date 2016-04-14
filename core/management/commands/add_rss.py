e908bcd58bc6c4efe8e2918f3dc9285dd6239b43
Gabe Wachob <gabe@tapcentive.com>
2016-04-14 10:58:18 -0700

    PEP8 fixes

--- core/management/commands/add_rss.py
+++ core/management/commands/add_rss.py
@@ -16,13 +16,14 @@ class Command(BaseCommand):
         entered_source = EnteredSource.objects.filter(source_type=EnteredSource.TYPE_RSS).first()
 
         if entered_source is not None:
-            self.stdout.write(self.style.SUCCESS("Archiving URL with EnteredSource id: {}".format(entered_source.id)))
+            self.stdout.write(self.style.SUCCESS("Archiving URL with EnteredSource id: {}".
+                                                 format(entered_source.id)))
             entered_source.source_type = EnteredSource.TYPE_ARCHIVED_RSS
             entered_source.save()
 
         # now create the new entered source
         entered_source = EnteredSource.objects.create(source_type=EnteredSource.TYPE_RSS,
-                                                        url=url, last_error=None)
+                                                      url=url, last_error=None)
         results = ingest_rss_source(entered_source)
 
         if 'error' in results:

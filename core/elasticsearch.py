754c9be79c0d9ce857febb4a0eedcd2cd98766e2
Gabe Wachob <gabe@tapcentive.com>
2016-04-27 17:38:43 -0700

    Added a reindex_all management command that rebuilds the elasticsearch index from scratch by iterating through all the docs in the db

--- core/elasticsearch.py
+++ core/elasticsearch.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 from threading import Lock
+import json
 
 _es = None
 _lock = Lock()
@@ -25,5 +26,22 @@ def index_document(document, doc_id, doc_type=settings.ELASTICSEARCH_TYPE):
         raise ValueError("settings.ELASTICSEARCH_INDEX is None")
     client.index(index=index,
                  doc_type=doc_type,
-                 id=doc_id,
+                 id=int(doc_id),
                  body=document)
+
+
+def bulk_index_documents(documents, doc_type=settings.ELASTICSEARCH_TYPE):
+    client = get_client()
+
+    index = settings.ELASTICSEARCH_INDEX
+    if index is None:
+        raise ValueError("settings.ELASTICSEARCH_INDEX is None")
+
+    actions = [
+        {'_type': doc_type, '_op_type': 'index', '_index': index,
+         '_id': int(doc.id), '_source': doc.as_json()} for doc in documents]
+
+    result = helpers.bulk(client, actions)
+    return result
+
+
---
+++ core/management/commands/reindex_all.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from core.models import Content
+import sys
+from core.elasticsearch import bulk_index_documents
+from tqdm import tqdm
+
+class Command(BaseCommand):
+
+    help = ('Reindexes all documents. Does not remove existing documents, but rather overwrites by index')
+
+    def handle(self, *args, **options):
+        confirm = None
+        while confirm not in ('y', 'Y'):
+            confirm = input("Do you want to reindex all content in elasticsearch (y/N): ")
+            if confirm in ('n', 'N', ''):
+                sys.exit(0)
+
+        contents = Content.objects.all()
+        total = 0
+        n = 50
+        with tqdm(total=contents.count()) as pbar:
+            for chunk in [contents[i:i+n] for i in range(0, contents.count(), n)]:
+                bulk_index_documents(chunk)
+                pbar.update(len(chunk))
+
+        self.stdout.write(self.style.SUCCESS("Uploaded stuff"))

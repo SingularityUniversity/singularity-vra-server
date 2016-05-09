aff256d51630425e99788d23d7a905f26b7520dd
Mike Lindelsee <mike@tapcentive.com>
2016-05-09 15:12:51 -0700

    Initial refactoring to support production builds -- local builds work correctly

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -7,27 +7,15 @@ var ASSETS_DIR = path.resolve(__dirname, 'assets/');
 var BUNDLE_DIR = ASSETS_DIR + '/bundles/';
 
 var config = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    ASSETS_DIR + '/js/index',
-  ],
+  entry: './assets/js/index',
   output: {
     path: BUNDLE_DIR,
     filename: '[name]-[hash].js',
-    publicPath: 'http://localhost:3000/assets/bundles/',
   },
   module: {
     context: __dirname,
     entry: ASSETS_DIR + 'js/index',
     loaders: [
-      {
-        test: /\.jsx?$/,
-        include: ASSETS_DIR,
-        exclude: /node_modules/,
-        loaders: ['react-hot', 'babel?'+JSON.stringify({presets: ["es2016", "es2015", "stage-0", "react"]})]
-      },
       {
         test: /(\.scss|\.sass| \.css)$/,
             loader: "style-loader!css-loader?sourceMap&importLoaders=1!postcss-loader!sass?sourceMap"
@@ -38,19 +26,7 @@ var config = {
       }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new BundleTracker(
-      {
-        path: ASSETS_DIR,
-        filename: '/webpack-stats.json'
-      }
-    ),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
+  plugins: [],
   postcss: [autoprefixer],
   resolve: {
     modulesDirectories: ['node_modules'],
---
+++ webpack.local.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack')
+var path = require('path')
+var BundleTracker = require('webpack-bundle-tracker')
+var autoprefixer = require('autoprefixer');
+
+var config = require('./webpack.base.config.js')
+
+var ASSETS_DIR = path.resolve(__dirname, 'assets/');
+var BUNDLE_DIR = ASSETS_DIR + '/bundles/';
+
+config.devtool = 'source-map'
+
+config.entry = [
+  'webpack-dev-server/client?http://localhost:3000',
+  'webpack/hot/only-dev-server',
+  ASSETS_DIR + '/js/index',
+]
+
+config.output.publicPath = 'http://localhost:3000/assets/bundles/'
+
+config.plugins = config.plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new BundleTracker(
+      {
+        path: ASSETS_DIR,
+        filename: '/webpack-stats.json'
+      }
+      ),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+])
+
+config.module.loaders.push({
+  test: /\.jsx?$/,
+  include: ASSETS_DIR,
+  exclude: /node_modules/,
+  loaders: ['react-hot', 'babel?'+JSON.stringify({presets: ["es2016", "es2015", "stage-0", "react"]})]
+})
+
+module.exports = config

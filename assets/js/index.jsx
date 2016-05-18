c6e8fcf8b27b3fb0cb7a1a5f813670cf56e6243b
Gabe Wachob <gabe@tapcentive.com>
2016-05-18 14:52:20 -0700

    Add CSRF header to all unsafe AJAX requests

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './app';
+import Cookies from 'js-cookie';
+import $ from 'jquery';
+
 
 require("../css/style.scss");
 
 // Needed for onTouchTap 
 // http://stackoverflow.com/a/34015469/988941 
 injectTapEventPlugin();
+
+let csrftoken = Cookies.get('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 render(<App/>, document.getElementById('app'))

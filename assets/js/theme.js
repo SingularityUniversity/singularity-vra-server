382b20e548939e43191ef18cdde43cb3eae200d0
Gabe Wachob <gabe@tapcentive.com>
2016-06-02 18:59:37 -0700

    Reformt to 4 spaces

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,104 +11,104 @@ const {desktopGutter, desktopKeylineIncrement} = spacing;
 const {fontWeightLight} = typography; 
 
 export default {
-  spacing: spacing,
-  fontFamily: 'Roboto, sans-serif',
-  tableRowColumn: {
-    spacing: 8
-  },
-  palette: {
-    primary1Color: colors.blueGrey500,
-    primary2Color: colors.blueGrey700,
-    primary3Color: colors.blueGrey900,
-    accent1Color: colors.indigoA200,
-    accent2Color: colors.grey100,
-    accent3Color: colors.grey500,
-    textColor: colors.darkBlack,
-    alternateTextColor: colors.white,
-    canvasColor: colors.white,
-    borderColor: colors.grey300,
-    disabledColor: fade(colors.darkBlack, 0.3),
-    pickerHeaderColor: colors.blueGrey500,
-    clockCircleColor: fade(colors.darkBlack, 0.07),
-    shadowColor: colors.fullBlack
-  },
-  logo: {
-      cursor: 'pointer',
-      fontSize: 24,
-      color: colors.textFullWhite,
-      lineHeight: desktopKeylineIncrement + 'px',
-      fontWeight: fontWeightLight,
-      backgroundColor: colors.cyan500,
-      paddingLeft: desktopGutter,
-      marginBottom: 8
+    spacing: spacing,
+    fontFamily: 'Roboto, sans-serif',
+    tableRowColumn: {
+        spacing: 8
     },
-  appBar: {
-      position: 'fixed',
-      top: 0,
-      zIndex: zIndex.appBar +1,
-      paddingTop: '0px',
-      paddingBottom: '0px',
-      paddingLeft: desktopGutter,
-      paddingRight: desktopGutter,
-    content: {
-      a: {
-        color: colors.darkWhite
-      },
-      p: {
-        margin: '0 auto',
-        padding: 0,
-        color: colors.lightWhite,
-        maxWidth: 335
-      },
-      iconButton: {
-        color: colors.darkWhite
-      },
-      content: {
-        margin: desktopGutter
-      }
-}
-  },
-  leftNav: {
-      zIndex: zIndex.appBar -1,
-      paddingTop: desktopKeylineIncrement, 
-      headerHeight: desktopKeylineIncrement*2
-  },
-  masterBar: {
-    contentWhenMedium: {
-      margin: `${desktopGutter * 2}px ${desktopGutter * 3}px`
+    palette: {
+        primary1Color: colors.blueGrey500,
+        primary2Color: colors.blueGrey700,
+        primary3Color: colors.blueGrey900,
+        accent1Color: colors.indigoA200,
+        accent2Color: colors.grey100,
+        accent3Color: colors.grey500,
+        textColor: colors.darkBlack,
+        alternateTextColor: colors.white,
+        canvasColor: colors.white,
+        borderColor: colors.grey300,
+        disabledColor: fade(colors.darkBlack, 0.3),
+        pickerHeaderColor: colors.blueGrey500,
+        clockCircleColor: fade(colors.darkBlack, 0.07),
+        shadowColor: colors.fullBlack
     },
-    footer: {
-      backgroundColor: colors.grey900,
-      textAlign: 'center'
+    logo: {
+        cursor: 'pointer',
+        fontSize: 24,
+        color: colors.textFullWhite,
+        lineHeight: desktopKeylineIncrement + 'px',
+        fontWeight: fontWeightLight,
+        backgroundColor: colors.cyan500,
+        paddingLeft: desktopGutter,
+        marginBottom: 8
     },
-    a: {
-      color: colors.darkWhite
+    appBar: {
+        position: 'fixed',
+        top: 0,
+        zIndex: zIndex.appBar +1,
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        paddingLeft: desktopGutter,
+        paddingRight: desktopGutter,
+        content: {
+            a: {
+                color: colors.darkWhite
+            },
+            p: {
+                margin: '0 auto',
+                padding: 0,
+                color: colors.lightWhite,
+                maxWidth: 335
+            },
+            iconButton: {
+                color: colors.darkWhite
+            },
+            content: {
+                margin: desktopGutter
+            }
+        }
     },
-    p: {
-      margin: '0 auto',
-      padding: 0,
-      color: colors.lightWhite,
-      maxWidth: 335
+    leftNav: {
+        zIndex: zIndex.appBar -1,
+        paddingTop: desktopKeylineIncrement, 
+        headerHeight: desktopKeylineIncrement*2
     },
-    iconButton: {
-      color: colors.darkWhite
+    masterBar: {
+        contentWhenMedium: {
+            margin: `${desktopGutter * 2}px ${desktopGutter * 3}px`
+        },
+        footer: {
+            backgroundColor: colors.grey900,
+            textAlign: 'center'
+        },
+        a: {
+            color: colors.darkWhite
+        },
+        p: {
+            margin: '0 auto',
+            padding: 0,
+            color: colors.lightWhite,
+            maxWidth: 335
+        },
+        iconButton: {
+            color: colors.darkWhite
+        }
+    },
+    fullWidthSection: {
+        item: {
+            padding: desktopGutter + 'px',
+            boxSizing: 'border-box',
+            paddingTop: desktopGutter,
+            paddingBottom: desktopGutter,
+            marginTop: desktopGutter,
+            marginBottom: desktopGutter
+        },
+        root: {
+            maxWidth: '1200px',
+            marginTop: desktopKeylineIncrement * 2,
+            marginRight: desktopKeylineIncrement,
+            marginBottom: desktopKeylineIncrement,
+            marginLeft: desktopGutter + (desktopKeylineIncrement * 4)
+        }
     }
-  },
-  fullWidthSection: {
-    item: {
-      padding: desktopGutter + 'px',
-      boxSizing: 'border-box',
-      paddingTop: desktopGutter,
-      paddingBottom: desktopGutter,
-      marginTop: desktopGutter,
-      marginBottom: desktopGutter
-    },
-    root: {
-      maxWidth: '1200px',
-      marginTop: desktopKeylineIncrement * 2,
-      marginRight: desktopKeylineIncrement,
-      marginBottom: desktopKeylineIncrement,
-      marginLeft: desktopGutter + (desktopKeylineIncrement * 4)
-    }
-  }
 };

4b4f455304241dd89d1e3a62d86efb69f2c6b408
Gabe Wachob <gabe@tapcentive.com>
2016-07-06 14:48:48 -0700

    Add 'preprocess_all' manage.py command to re-invoke the preprocessing functions on all content

---
+++ core/management/commands/preprocess_all.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from core.models import Content
+import sys
+from tqdm import tqdm
+
+class Command(BaseCommand):
+
+    help = ('Repeats preprocessing steps for all documents.')
+
+    def handle(self, *args, **options):
+        confirm = None
+        while confirm not in ('y', 'Y'):
+            confirm = input("Do you want to repreprocess all content (y/N): ")
+            if confirm in ('n', 'N', ''):
+                sys.exit(0)
+
+        contents = Content.objects.all()
+
+        for content in tqdm(contents):
+            content.save()
+
+        self.stdout.write(self.style.SUCCESS("Re-preprocessed all documents"))

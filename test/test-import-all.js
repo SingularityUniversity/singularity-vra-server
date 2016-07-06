25c6c38767efafdc9020530515d69f958ee9b452
Gabe Wachob <gabe@tapcentive.com>
2016-07-06 14:45:56 -0700

    Fix test-import-all after having renamed app-left-nav

--- test/test-import-all.js
+++ test/test-import-all.js
@@ -1,9 +1,6 @@
 // This file makes the coverage tool import all files (really probably we only need to import master)
 // so coverage reporting is accurate for files that aren't tested at all
 import Master from '../assets/js/containers/master';
-import AppLeftNav from '../assets/js/components/app-left-nav';
-import AppMenuBar from '../assets/js/components/app-menu-bar';
-import Workspace from '../assets/js/components/workspace';
 
 // Importing app.jsx causes issues because of the import 'react-virtualized/styles.css' statement
 // See the following if relevant:
